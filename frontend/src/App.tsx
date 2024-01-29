d3621e7e12c3b11928c4e43fa8ed0f33feb2c69f
Jacob Krch <jekrch@gmail.com>
2024-01-28 18:13:41 -0600

    experimenting with vexflow and tonal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,156 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { ChordControllerService } from './api/services/ChordControllerService';
 import { ModeControllerService } from './api/services/ModeControllerService';
 import { ModeDto } from './api/models/ModeDto';
-
+import Dropdown from './components/Dropdown';
+import Vex from 'vexflow';
+import { Chord } from 'tonal'
 
 function App() {
-
   const [refresh, setRefresh] = useState(0);
   const [chords, setChords] = useState(null);
+  const [selectedMode, setSelectedMode] = useState<string>('Dorian');
   const [error, setError] = useState(null);
-  const [modes, setModes] = useState<ModeDto[] | undefined>();
+  const [modes, setModes] = useState<string[] | undefined>();
+  const notationRef = useRef<HTMLDivElement>(null);
+  const tabNotationRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     ModeControllerService.getModes()
-        .then((response) => {
-            setModes(response); 
-            setRefresh(Math.random());
-            console.log(response);
-        })
-        .catch((err) => {
-            setError(err);
-            console.error('Error fetching modes:', err);
-        });
-}, []);
+      .then((response) => {
+        setModes(response.map(m => m.name!));
+        setRefresh(Math.random());
+        console.log(response);
+      })
+      .catch((err) => {
+        setError(err);
+        console.error('Error fetching modes:', err);
+      });
+  }, []);
+
+  useEffect(() => {
+    ChordControllerService.getChords()
+      .then((response) => {
+        setChords(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+        console.error('Error fetching chords:', err);
+      });
+  }, []);
+
+  const getChordNotes = (chordName: string) => {
+    return Chord.get(chordName).notes; // Returns an array of note names
+};
+
+  useEffect(() => {
+    if (notationRef.current) {
+      const notes = getChordNotes('C');
+      console.log(notes)
+
+      notationRef.current.innerHTML = '';
+      const VF = Vex.Flow;
+      const div = notationRef.current;
+      const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+      renderer.resize(300, 200);
+      const context = renderer.getContext();
+      context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+      const stave = new VF.Stave(10, 40, 400);
+      stave.addClef("treble").addTimeSignature("4/4");
+      stave.setContext(context).draw();
+
+      const cMajorChord = new VF.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "w" });
+
+      const voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
+      voice.addTickables([cMajorChord]);
+
+      const formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+      voice.draw(context, stave);
+
+    }
+  }, []);
 
     useEffect(() => {
-      ChordControllerService.getChords()
-          .then((response) => {
-              setChords(response.data); 
-          })
-          .catch((err) => {
-              setError(err);
-              console.error('Error fetching chords:', err);
-          });
-  }, []);
+        if (tabNotationRef.current) {
+          tabNotationRef.current.innerHTML = '';
+            const VF = Vex.Flow;
+            const div = tabNotationRef.current;
+            const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+            renderer.resize(400, 250);
+            const context = renderer.getContext();
+            context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+            const stave = new VF.Stave(10, 40, 400);
+            stave.addClef("treble").addTimeSignature("4/4");
+            stave.setContext(context).draw();
+
+            // tab
+            const tabStave = new VF.TabStave(10, 100, 400);
+            tabStave.addClef("tab").setContext(context).draw();
+
+            const cMajorChord = new VF.StaveNote({
+                keys: ["c/4", "e/4", "g/4"],
+                duration: "w"
+            });
+
+            // Chord for guitar tablature
+            const cMajorChordTab = new VF.TabNote({
+                positions: [{str: 5, fret: 3}, {str: 4, fret: 2}, {str: 3, fret: 0}], // C Major chord positions
+                duration: "w"
+            });
+
+            // Create voices and add notes
+            const voice = new VF.Voice({num_beats: 4, beat_value: 4}).addTickables([cMajorChord]);
+            const tabVoice = new VF.Voice({num_beats: 4, beat_value: 4}).addTickables([cMajorChordTab]);
+
+            // Format and justify the notes to 400 pixels
+            new VF.Formatter().joinVoices([voice]).format([voice], 400);
+            new VF.Formatter().joinVoices([tabVoice]).format([tabVoice], 400);
+
+            // Render voices
+            voice.draw(context, stave);
+            tabVoice.draw(context, tabStave);
+        }
+    }, []);
+
 
   return (
     <div className="App">
       <header className="App-header">
-       
+        {modes &&
+          <Dropdown
+            value={selectedMode}
+            className='w-auto min-w-[10em]'
+            menuClassName='min-w-[10em]'
+            onChange={(v) => { setSelectedMode(v) }}
+            showSearch={true}
+            options={modes}
+          />
+        }
+        <div className="bg-white mt-[2em]">
+          <div className="" ref={notationRef} />
+        </div>
+        <div className="bg-white mt-[2em]">
+          <div className="" ref={tabNotationRef} />
+        </div>
         <p key={`modes=${refresh}`}>
-          {modes?.map((mode: ModeDto, index: number) => (
+
+
+          {/* {modes?.map((mode: string, index: number) => (
             <div 
               key={`mode-${index}`}
               className="text-slate-200"
             >
-                {mode.name}
+                {mode}
               </div>
-          ))}
+          ))} */}
         </p>
-       
+
       </header>
     </div>
   );
---
+++ frontend/src/components/Dropdown.tsx
@@ -0,0 +1,135 @@
+import React, { useRef, useState } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import classNames from 'classnames';
+import { createPortal } from 'react-dom';
+
+type DropdownProps = {
+  className?: string;
+  menuClassName?: string;
+  buttonClassName?: string;
+  value: string;
+  onChange: (value: string) => void;
+  options: string[];
+  showSearch: boolean;
+};
+
+const Dropdown: React.FC<DropdownProps> = ({ value, onChange, options, className, menuClassName, showSearch, buttonClassName }) => {
+  const [filter, setFilter] = useState('');
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [menuPosition, setMenuPosition] = useState({
+    top: 0, 
+    left: 0, 
+    maxHeight: 300 
+  });
+  const filteredOptions = options.filter(option =>
+    filter?.length ? option.toLowerCase().includes(filter.toLowerCase()) : true
+  );
+
+  const portalMountNode = document.body;
+
+  const handleMenuClose = () => {
+    setFilter('');
+  };
+
+  const updateMenuPosition = () => {
+    if (buttonRef.current) {
+      const rect = buttonRef.current.getBoundingClientRect();
+      const bottomSpace = window.innerHeight - rect.bottom; 
+      const maxHeight = Math.min(bottomSpace - 10, 300); 
+  
+      setMenuPosition({
+        top: rect.bottom + window.scrollY,
+        left: rect.left + window.scrollX,
+        maxHeight 
+      });
+    }
+  };
+  
+
+  return (
+    <Menu as="div" className={classNames("relative inline-block text-left z-50", className)}>
+      <div>
+        <Menu.Button 
+          ref={buttonRef}
+          onClick={(e) => {
+            updateMenuPosition();
+          }}
+          className={classNames(
+            "inline-flex w-full justify-center gap-x-1.5 rounded-md",
+            "bg-slate-700 bg-opacity-10 px-3 py-[0.2em] h-6 text-sm font-bold",
+            "text-gray-400 shadow-sm ring-1 ring-inset ring-gray-400 hover:bg-opacity-30",
+            "h-[2em] items-center", 
+            buttonClassName
+          )}>
+          {value}
+          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        {createPortal(
+          <div
+          style={{
+            position: 'absolute',
+            top: `${menuPosition.top}px`,
+            left: `${menuPosition.left}px`,
+            zIndex: 1000,
+          }}
+          className="origin-top-right"
+        >
+        <Menu.Items 
+          as="div" 
+          style={{ maxHeight: `${menuPosition.maxHeight}px`, overflowY: 'auto' }}
+          className="dropdown-menu absolute mt-2 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none custom-scrollbar w-auto"
+         >
+          <div className="py-1 bg-slate-600 bg-opacity-96">
+            {showSearch &&
+              <input
+                type="text"
+                className="w-full text-slate-300 px-4 py-2 text-sm font-normal bg-slate-800"
+                placeholder="Search..."
+                value={filter}
+                onChange={(e) => {
+                  setFilter(e.target.value)
+                }}
+              />
+            }
+            <div className={classNames("overflow-y-auto", menuClassName)}>
+              {filteredOptions.map((option, index) => (
+                <Menu.Item key={index}>
+                  {({ active }) => (
+                    <button
+                      onClick={(event) => {
+                        onChange(option)
+                        setFilter('');
+                      }}
+                      className={classNames(
+                        active ? 'bg-slate-400 text-blue-100' : 'text-slate-300',
+                        'block w-full px-4 py-2 text-left text-sm'
+                      )}
+                    >
+                      {option}
+                    </button>
+                  )}
+                </Menu.Item>
+              ))}
+            </div>
+          </div>
+        </Menu.Items>
+         </div>,
+        portalMountNode
+        )}
+      </Transition>
+    </Menu>
+  );
+};
+
+export default Dropdown;

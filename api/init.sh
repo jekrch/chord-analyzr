2e2aa889f0a83543574650c981c328f6b195e3ff
Jacob Krch <jekrch@gmail.com>
2024-03-03 17:32:23 -0600

    fix docker-copose: force api to wait for flyway

---
+++ api/init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DB_HOST="postgres"
+DB_PORT=${SPRING_DATASOURCE_PORT}
+DB_USER=${SPRING_DATASOURCE_USERNAME}
+DB_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
+DB_NAME=${SPRING_DATASOURCE_DB}
+TABLE_NAME="mode_scale_chord_relation_mv"
+
+# wait for the largest materialized view to load (which comes last in flyway)
+until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -p "$DB_PORT" -c "SELECT 1 FROM $TABLE_NAME LIMIT 1;" &> /dev/null
+do
+  echo "Waiting for Flyway migrations to be applied..."
+  sleep 1
+done
+
+echo "Database is ready."
+
+# now start the api
+exec java -Xverify:none -jar /usr/src/app/api.jar
